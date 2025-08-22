054a71b8e956c212de6bf7601494f7ff007a0d96
Jarrod Norwell <official.antique@gmail.com>
2025-08-22 10:54:45 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "libseekable_format",
+            dependencies: [ "libzstd" ],
             path: "contrib",
             sources: [ "seekable_format" ],
             publicHeadersPath: ".",
